77c709fa98354e4a5c81f8c383c8782d37e696e4
shahul01 <shahulhammeed01@gmail.com>
2022-01-10 21:53:49 +0530

    chore: updated Tailwind and ESlint version

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-
+  let name:string = "world";
+  // name = 123
 </script>
 
 <div class="my-container custom-global-class">
   <p class="m-4 p-2 font-bold">
-    Hello world
+    Hello {name}
 
   </p>
 
